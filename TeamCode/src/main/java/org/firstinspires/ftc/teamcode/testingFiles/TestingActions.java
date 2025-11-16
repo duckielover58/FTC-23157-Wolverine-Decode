1f4415d0962fe525997a20e601507c0e9cf87cb8
duckielover58 <happyvak@gmail.com>
2025-11-16 12:27:01 -0800

    Moved TestingActions.java into new package testingFiles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/TestingActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/TestingActions.java
@@ -1,40 +1,17 @@
-package org.firstinspires.ftc.teamcode.driveClasses;
-
-import android.graphics.HardwareRenderer;
-
-import androidx.annotation.NonNull;
+package org.firstinspires.ftc.teamcode.testingFiles;
 
 // RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ParallelAction;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.TrajectoryBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 
 // Non-RR imports
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
+        import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
 import org.firstinspires.ftc.teamcode.subsystems.Index;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Push;
 import org.firstinspires.ftc.teamcode.subsystems.Swivel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @TeleOp
 public class TestingActions extends LinearOpMode {
