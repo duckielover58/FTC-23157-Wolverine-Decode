55b956599fb12507ce4c372626ab56f0519db943
duckielover58 <happyvak@gmail.com>
2025-11-16 12:35:43 -0800

    Added ShooterLockTest.java & AprilTagGameDatabaseEditable.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/ShooterLockTest.java
@@ -0,0 +1,173 @@
+package org.firstinspires.ftc.teamcode.testingFiles;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp public class ShooterLockTest extends LinearOpMode {
+    private Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 8.5, 1, 0);
+    //TODO Measure with tape
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+
+    /**
+     * The variable to store our instance of the AprilTag processor.
+     */
+    private AprilTagProcessor aprilTag;
+
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private VisionPortal visionPortal;
+
+    private double robotHeading = 0; //placeholder
+    private double cameraHeading = robotHeading;
+    private double ServoPower = 0;
+    @Override
+    public void runOpMode() {
+        initAprilTag();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetryAprilTag();
+            telemetry.update();
+
+            //servo.setPosition(turnPosition);
+
+            // Share the CPU.
+            sleep(20);
+
+            if (isStopRequested()) {
+                visionPortal.close();
+            }
+        }
+        visionPortal.close();
+    }
+
+    private void initAprilTag() {
+
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+                //Cam visual settings
+                // The following default settings are available to un-comment and edit as needed.
+                //.setDrawAxes(false)
+                //.setDrawCubeProjection(false)
+                //.setDrawTagOutline(true)
+                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabaseEditable.getCurrentGameTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .setCameraPose(cameraPosition, cameraOrientation)
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+                // ... these parameters are fx, fy, cx, cy.
+                .build();
+
+        //Cam fps
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        //aprilTag.setDecimation(3);
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        //builder.enableLiveView(true);
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
+        builder.setAutoStopLiveView(false);
+
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+
+        visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }
+
+    private double bearing;
+
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.id != 0) {
+
+                if (detection.ftcPose.bearing > 0) {
+                    ServoPower = -1;
+                } else if (detection.ftcPose.bearing < 0) {
+                    ServoPower = 1;
+                } else if (detection.ftcPose.bearing + -0.1 == 0) {
+                    ServoPower = 0;
+                }
+
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addData("Servo Power:", ServoPower);
+                telemetry.addData("Bearing:", detection.ftcPose.bearing);
+
+            }
+
+            // Add "key" information to telemetry
+            telemetry.addData("Servo Power:", ServoPower);
+            telemetry.addData("Bearing:", bearing);
+            telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+            telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+
+            sleep(20);
+        }
+    }
+
+
+/*
+    private void telemetryAprilTag() {
+
+    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+    telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+    // Step through the list of detections and display info for each one.
+    for (AprilTagDetection detection : currentDetections) {
+        if (detection.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+                    detection.robotPose.getPosition().x,
+                    detection.robotPose.getPosition().y,
+                    detection.robotPose.getPosition().z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+                    detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+                    detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+                    detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+        }
+    }
+
+    // Add "key" information to telemetry
+    telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+    telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+
+}
+*/
+}
