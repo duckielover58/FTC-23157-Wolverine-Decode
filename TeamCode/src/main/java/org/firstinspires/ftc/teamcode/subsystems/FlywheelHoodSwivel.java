2f798f41684db50e501367aa14b0853bf784a609
gautam <iamaroragautam@gmail.com>
2025-11-19 19:41:08 -0800

    created new files for testing all motors and servos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelHoodSwivel.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class FlywheelHoodSwivel {
+    private DcMotor Flywheel;
+    private Servo Hood;
+    private Servo Swivel;
+}
