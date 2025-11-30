ac75a6d6bccc5ddd0fba1ee96e2ee7aecd240545
gautam <iamaroragautam@gmail.com>
2025-11-30 12:35:19 -0800

    linearopmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/TestTeleOp.java
@@ -1,15 +1,12 @@
 package org.firstinspires.ftc.teamcode.testingFiles;
 
-import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.firstinspires.ftc.teamcode.driveClasses.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
 import org.firstinspires.ftc.teamcode.subsystems.Index;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
@@ -17,7 +14,7 @@ import org.firstinspires.ftc.teamcode.subsystems.Push;
 import org.firstinspires.ftc.teamcode.subsystems.Swivel;
 
 @TeleOp(name = "TestTeleOp")
-public class TestTeleOp extends OpMode {
+public class TestTeleOp extends LinearOpMode {
 
     private boolean pushedUp = false;
 
@@ -33,7 +30,9 @@ public class TestTeleOp extends OpMode {
     Index index;
 
     @Override
-    public void init() {
+    public void runOpMode() {
+
+        // --- INIT MOTORS ---
         leftFront  = hardwareMap.get(DcMotor.class, "FrontLeft");
         rightFront = hardwareMap.get(DcMotor.class, "FrontRight");
         leftBack   = hardwareMap.get(DcMotor.class, "BackLeft");
@@ -44,73 +43,102 @@ public class TestTeleOp extends OpMode {
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
 
+        // --- INIT SUBSYSTEMS ---
         intake = new Intake(hardwareMap);
         push = new Push(hardwareMap);
         flywheel = new Flywheel(hardwareMap);
         swivel = new Swivel(hardwareMap);
         index = new Index(hardwareMap);
-    }
-    //hello
 
-    @Override
-    public void loop() {
+        telemetry.addLine("Initialized");
+        telemetry.update();
 
-        double vertical   = -gamepad1.left_stick_y;
-        double horizontal = gamepad1.left_stick_x;
-        double pivot      = gamepad1.right_stick_x;
+        waitForStart();
 
-        double frontLeftPower  = vertical + horizontal + pivot;
-        double frontRightPower = vertical + horizontal - pivot;
-        double backLeftPower   = vertical - horizontal + pivot;
-        double backRightPower  = vertical - horizontal - pivot;
+        while (opModeIsActive()) {
 
-        double max = Math.max(Math.abs(frontLeftPower),
-                Math.max(Math.abs(frontRightPower),
-                        Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+            double vertical   = -gamepad1.left_stick_y;
+            double horizontal = gamepad1.left_stick_x;
+            double pivot      = gamepad1.right_stick_x;
 
-        if (max > 1.0) {
-            frontLeftPower  /= max;
-            frontRightPower /= max;
-            backLeftPower   /= max;
-            backRightPower  /= max;
-        }
+            double frontLeftPower  = vertical + horizontal + pivot;
+            double frontRightPower = vertical + horizontal - pivot;
+            double backLeftPower   = vertical - horizontal + pivot;
+            double backRightPower  = vertical - horizontal - pivot;
 
-        leftFront.setPower(frontLeftPower);
-        rightFront.setPower(frontRightPower);
-        leftBack.setPower(backLeftPower);
-        rightBack.setPower(backRightPower);
+            double max = Math.max(Math.abs(frontLeftPower),
+                    Math.max(Math.abs(frontRightPower),
+                            Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
 
-        if (gamepad1.a) {
-            Actions.runBlocking(intake.IntakeBall());
-        }
-        if (gamepad1.left_bumper) {
-            Actions.runBlocking(intake.IntakeBallStop());
-        }
-        if (gamepad1.b && !pushedUp) {
-            Actions.runBlocking(push.PushBallUp());
-            pushedUp = true;
-        } else {
-            Actions.runBlocking(push.PushBallDown());
-            pushedUp = false;
-        }
-        if (gamepad1.y) {
-            Actions.runBlocking(flywheel.shoot());
-        }
-        if (gamepad1.x) {
-            Actions.runBlocking(index.index1());
-        }
-        if (gamepad1.dpad_up) {
-            Actions.runBlocking(index.index2());
-        }
-        if (gamepad1.dpad_left) {
-            Actions.runBlocking(index.index3());
-        }
-        if (gamepad1.dpad_right) {
-            Actions.runBlocking(swivel.aim());
-        }
-        if (gamepad1.right_bumper) {
-            Actions.runBlocking(flywheel.shootStop());
-        }
+            if (max > 1.0) {
+                frontLeftPower  /= max;
+                frontRightPower /= max;
+                backLeftPower   /= max;
+                backRightPower  /= max;
+            }
 
+            leftFront.setPower(frontLeftPower);
+            rightFront.setPower(frontRightPower);
+            leftBack.setPower(backLeftPower);
+            rightBack.setPower(backRightPower);
+
+
+            if (gamepad1.a) {
+                Actions.runBlocking(intake.IntakeBall());
+                sleep(500);
+            }
+            if (gamepad1.left_bumper) {
+                Actions.runBlocking(intake.IntakeBallStop());
+                sleep(500);
+            }
+            if (gamepad1.b) {
+                Actions.runBlocking(push.PushBallUp());
+                sleep(500);
+            }
+            if (gamepad1.dpad_down) {
+                Actions.runBlocking(push.PushBallDown());
+                sleep(500);
+            }
+/*
+            if (gamepad1.b && !pushedUp) {
+                Actions.runBlocking(push.PushBallUp());
+                sleep(100);
+                pushedUp = true;
+            }
+            else if (pushedUp == true) {
+                Actions.runBlocking(push.PushBallDown());
+                sleep(100);
+                pushedUp = false;
+            }
+
+ */
+
+            if (gamepad1.y) {
+                Actions.runBlocking(flywheel.shoot());
+                sleep(500);
+            }
+            if (gamepad1.x) {
+                Actions.runBlocking(index.index1());
+                sleep(500);
+            }
+            if (gamepad1.dpad_up) {
+                Actions.runBlocking(index.index2());
+                sleep(500);
+            }
+            if (gamepad1.dpad_left) {
+                Actions.runBlocking(index.index3());
+                sleep(500);
+            }
+            if (gamepad1.dpad_right) {
+                Actions.runBlocking(swivel.aim());
+                sleep(500);
+            }
+            if (gamepad1.right_bumper) {
+                Actions.runBlocking(flywheel.shootStop());
+                sleep(500);
+            }
+
+            telemetry.update();
+        }
     }
 }
