76570463b5afae1cb8bce30f13fa4618329dcdff
duckielover58 <happyvak@gmail.com>
2025-11-30 20:20:03 -0800

    Added a new version of TestTeleOp with different movement controls.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/TestTeleOpv2.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode.testingFiles;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.driveClasses.PinpointDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
+import org.firstinspires.ftc.teamcode.subsystems.Index;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Push;
+import org.firstinspires.ftc.teamcode.subsystems.Swivel;
+
+@TeleOp(name = "TestTeleOpv2")
+public class TestTeleOpv2 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        PinpointDrive drive = new PinpointDrive(hardwareMap, new Pose2d(0,0,0));
+
+        Intake intake = new Intake(hardwareMap);
+        Push push = new Push(hardwareMap);
+        Flywheel flywheel = new Flywheel(hardwareMap);
+        Swivel swivel = new Swivel(hardwareMap);
+        Index index = new Index(hardwareMap);
+
+        telemetry.addLine("Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            drive.setDrivePowers(new PoseVelocity2d(new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x), -gamepad1.right_stick_x));
+
+            drive.updatePoseEstimate();
+
+            if (gamepad1.a) {
+                Actions.runBlocking(intake.IntakeBall());
+            }
+            if (gamepad1.left_bumper) {
+                Actions.runBlocking(intake.IntakeBallStop());
+            }
+            if (gamepad1.b) {
+                Actions.runBlocking(push.PushBallUp());
+            }
+            if (gamepad1.dpad_down) {
+                Actions.runBlocking(push.PushBallDown());
+            }
+            if (gamepad1.y) {
+                Actions.runBlocking(flywheel.shoot());
+            }
+            if (gamepad1.x) {
+                Actions.runBlocking(index.index1());
+            }
+            if (gamepad1.dpad_up) {
+                Actions.runBlocking(index.index2());
+            }
+            if (gamepad1.dpad_left) {
+                Actions.runBlocking(index.index3());
+            }
+            if (gamepad1.dpad_right) {
+                Actions.runBlocking(swivel.aim());
+            }
+            if (gamepad1.right_bumper) {
+                Actions.runBlocking(flywheel.shootStop());
+            }
+
+            telemetry.update();
+        }
+    }
+}
+
