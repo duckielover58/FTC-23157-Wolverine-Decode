85aefb9ac00f8b4706c824c7debc5ab2825dda80
Advika Mangal <advika.mangal@gmail.com>
2025-12-09 16:53:07 -0800

    edited both files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/LimelightAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/LimelightAprilTag.java
@@ -2,52 +2,33 @@ package org.firstinspires.ftc.teamcode.testingFiles;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
-
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
-public class LimelightAprilTag extends OpMode {
+public class LimelightAprilTag extends LinearOpMode {
 
-    private Limelight3A limelight3A;
-    private IMU imu;
-
-    @Override
-    public void init(){
-        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight3A.pipelineSwitch(8); //tbd need to change dpdening on april tag pipeline (i think its 0, 1, or 2)
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-        );
-        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
-    }
+    private Limelight3A limelight;
 
     @Override
-    public void start(){
-        limelight3A.start();
-    }
+    public void runOpMode() throws InterruptedException
+    {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(1);
+        limelight.start();
 
-    @Override
-    public void loop(){
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight3A.updateRobotOrientation(orientation.getYaw());
-        LLResult llResult = limelight3A.getLatestResult();
-        if (llResult != null && llResult.isValid()) {
-            Pose3D botpose = llResult.getBotpose_MT2(); //if not using imu remove _MT2 (metatag 2 so its default metatag 1)
-            telemetry.addData("Tx", llResult.getTx()); //target x
-            telemetry.addData("Ty", llResult.getTy()); //target y
-            telemetry.addData("Ta", llResult.getTa()); //target area
-
-
-
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
         }
     }
-
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/LimelightShooterLock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/LimelightShooterLock.java
@@ -16,27 +16,19 @@ import org.firstinspires.ftc.teamcode.subsystems.Swivel;
 public class LimelightShooterLock extends LinearOpMode {
 
     private Limelight3A limelight;
-    private IMU imu;
-    public static double ServoPower = 0;
+    public static double ServoPower = 1;
     public boolean servoLocked = false;
 
     // tuning constants
-    private final double bearingThreshold = 3; // same hting as bearing error
-    private final double servoSpeed = 0.1;
+    private final double bearingThreshold = 1; // same hting as bearing error
+    private final double servoSpeed = 0.3;
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Initialize Limelight
-        limelight = hardwareMap.get(Limelight3A.class, "Webcam 1");
-        limelight.pipelineSwitch(0); // adjust index for our specific AprilTag
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(1); // adjust index for our specific AprilTag
 
-        // Initialize IMU
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot revHubOrientation = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-        );
-        imu.initialize(new IMU.Parameters(revHubOrientation));
 
         Swivel swivel = new Swivel(hardwareMap);
 
@@ -45,13 +37,9 @@ public class LimelightShooterLock extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            // Updates Limelight orientation
-            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-            limelight.updateRobotOrientation(orientation.getYaw());
-
             LLResult result = limelight.getLatestResult();
             if (result != null && result.isValid()) {
-                Pose3D botpose = result.getBotpose_MT2(); // pose from Limelight
+                Pose3D botpose = result.getBotpose(); // pose from Limelight
                 double bearing = result.getTx(); // x offset in degrees from target (target x and bearing are the same thing i think)
 
                 // servo aiming/locking
