e2545312e11e25a7c07a3c91da962c294928e8e6
Sanshray Vakkalagadda <happyvak@gmail.com>
2025-12-20 21:48:41 -0800

    Feedforward loop to control flywheel speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/Flywheelgm0PIDtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingFiles/Flywheelgm0PIDtest.java
@@ -7,19 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 
 @Config
 @Autonomous(name = "Flywheelgm0PIDTest")
 public class Flywheelgm0PIDtest extends LinearOpMode {
 
-    // PID constants (START SMALL)
-    public static double kP = 0.05819;
-    public static double kI = 0.00002;
-    public static double kD = 0.010035;
-
-    public static double integralSum = 0;
-    public static double previousError = 0;
-    public static double previousTime = 0;
+    public static double kF = 0.00042;
+    public static double kP = 0.00009;
     public static double targetVelocity = 740;
 
     @Override
@@ -31,38 +26,35 @@ public class Flywheelgm0PIDtest extends LinearOpMode {
         flywheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         FtcDashboard Dashboard = FtcDashboard.getInstance();
         telemetry = Dashboard.getTelemetry();
+        TelemetryPacket packet = new TelemetryPacket();
 
         telemetry.addLine("Ready");
         telemetry.update();
 
         waitForStart();
 
-        previousTime = getRuntime();
-
         while (opModeIsActive()) {
 
-
             double currentVelocity = flywheel.getVelocity();
-            double currentTime = getRuntime();
-            double dt = currentTime - previousTime;
             double error = targetVelocity - currentVelocity;
 
-            integralSum += error * dt;
-            double derivative = (error - previousError) / dt;
+            double ff = kF * targetVelocity;
 
-            double output = (kP * error) + (kI * integralSum) + (kD * derivative);
-            output = Math.max(-1.0, Math.min(1.0, output));
+            double output = ff + (kP * error);
+
+            output = Math.max(0.0, Math.min(1.0, output));
 
             flywheel.setPower(output);
 
             telemetry.addData("Target Velocity: ", targetVelocity);
             telemetry.addData("Current Velocity: ", currentVelocity);
-            telemetry.addData("Error: ", error);
+            telemetry.addData("Erorr: ", error);
             telemetry.addData("Power: ", output);
+            packet.put("Target Velocity", targetVelocity);
+            packet.put("Current Velocity", currentVelocity);
+            packet.put("Error", error);
+            packet.put("Power", output);
             telemetry.update();
-
-            previousError = error;
-            previousTime = currentTime;
         }
     }
 }
